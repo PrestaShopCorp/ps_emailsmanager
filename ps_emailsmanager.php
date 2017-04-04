07882d6ba32f88053e6a7a95b1e8b79b1a55b897
MARICHAL Emmanuel <scritik@gmail.com>
2017-04-04 11:22:26 +0200

    Bump version to 1.2.1

--- ps_emailsmanager.php
+++ ps_emailsmanager.php
@@ -25,7 +25,7 @@ class Ps_EmailsManager extends Module
     public function __construct()
     {
         $this->name      = 'ps_emailsmanager';
-        $this->version   = '1.2.0';
+        $this->version   = '1.2.1';
         $this->tab       = 'emailing';
         $this->author    = 'PrestaShop';
         $this->bootstrap = true;
